18282bed2a86fa668f4fbce67e0ad1565f3fac2c
amit kumar <amitkumaramit118589@gmail.com>
2025-04-05 22:31:28 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.cpp
+++ Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.cpp
@@ -7,27 +7,39 @@ using namespace std;
 
 class Solution {
   public:
-    bool dfs(int i, int p, vector<vector<int>>&ed, vector<bool>&vis){
-        if(vis[i] == true) return true;
-        
-        vis[i] = true;
-        bool ok = false;
-        for(int j : ed[i]){
-            if(j == p) continue;
-            
-            ok = ok | dfs(j, i, ed, vis);
-        }
-        return ok;
-    }
     bool isCycle(int V, vector<vector<int>>& edges) {
         // Code here
-        vector<bool>vis(V, false);
-        for(int i = 0; i < V; i++){
-            if(vis[i] == true) continue;
-            
-            if(dfs(i,-1, edges, vis)) return true;
+        vector<int>adj[V];
+        for(auto &it:edges){
+            int u=it[0];
+            int v=it[1];
+            adj[u].push_back(v);
+            adj[v].push_back(u);
         }
-        return false;
+        
+        vector<bool>vis(V, 0);
+        vector<int>par(V, -1);
+        
+        for(int i=0; i<V; i++){
+            if(!vis[i]){
+                queue<int>q;
+                q.push(i);
+                vis[i]=1;
+                while(!q.empty()){
+                    int node=q.front();
+                    q.pop();
+                    
+                    for(auto &ngbr:adj[node]){
+                        if(!vis[ngbr]){
+                            vis[ngbr]=1;
+                            par[ngbr]=node;
+                            q.push(ngbr);
+                        }else if(par[node]!=ngbr) return 1;
+                    }
+                }
+            }
+        }
+        return 0;
     }
 };
 
