da80f01c9716fea997044af9e4d6b30384fc5798
amit kumar <amitkumaramit118589@gmail.com>
2025-12-02 20:54:37 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Minimum element in BST/minimum-element-in-bst.cpp
@@ -0,0 +1,25 @@
+/*
+class Node {
+  public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int x) {
+        data = x;
+        left = nullptr;
+        right = nullptr;
+    }
+};
+*/
+
+class Solution {
+  public:
+    int minValue(Node* root) {
+        // code here
+        if(root==NULL)return NULL;
+        if(root->left==NULL)return root->data;
+        minValue(root->left);
+        
+    }
+};
